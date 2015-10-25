7db672ca230c06d1057695707a3d9ce8fcc74a98
Rohan Benkar <rohanbenkar@gmail.com>
2015-10-25 02:08:17 -0700

    WIP

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -1,46 +1,32 @@
 package controllers;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.ArrayList;
-
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.simplify.payments.PaymentsApi;
 import com.simplify.payments.PaymentsMap;
 import com.simplify.payments.domain.Invoice;
 import com.simplify.payments.domain.Payment;
-import com.simplify.payments.exception.*;
+import model.Cart;
+import model.Product;
+import model.User;
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.geo.Point;
+import org.mongodb.morphia.geo.PointBuilder;
 import play.Logger;
 import play.libs.Json;
 import play.mvc.Result;
 import util.Config;
 import util.Utils;
-import model.User;
-import model.Cart;
-import org.bson.types.ObjectId;
 
-import org.mongodb.morphia.Datastore;
-import org.mongodb.morphia.geo.Point;
-import org.mongodb.morphia.geo.PointBuilder;
-import org.mongodb.morphia.query.Query;
-import org.mongodb.morphia.query.UpdateOperations;
-import org.mongodb.morphia.query.UpdateResults;
+public class UserController extends BaseController {
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
-public class UserController extends BaseController{
-
-	private static final String ACTIVITIES_TAG = "activities";
-
-    public static Result addUser(){
-        if(!Utils.checkJsonInput(request())){
+    public static Result addUser() {
+        if (!Utils.checkJsonInput(request())) {
             Logger.info("Bad request data to add product" + request().body());
             return generateBadRequest("Bad input json");
         }
-        
+
         JsonNode jsonReq = request().body().asJson();
         String name = Utils.safeStringFromJson(jsonReq, "name");
         String email = Utils.safeStringFromJson(jsonReq, "email");
@@ -49,12 +35,11 @@ public class UserController extends BaseController{
         long latitude = Utils.safeLongFromJson(jsonReq, "lat");
         long longitude = Utils.safeLongFromJson(jsonReq, "lon");
         Point currentLocation = new PointBuilder().latitude(latitude).longitude(longitude).build();
-        String status = Utils.safeStringFromJson(jsonReq, "status");
-        String level = Utils.safeStringFromJson(jsonReq, "level");
-        String segment = Utils.safeStringFromJson(jsonReq, "segment");
-        List<Cart> purchaseHistory = new ArrayList<Cart>();
-        
-        User user = new User(name, email, zip, purchaseHistory, purchasingPower, currentLocation, status, level, segment);
+        String status = "new";
+        String level = "normal";
+        String segment = "normal";
+
+        User user = new User(name, email, zip, purchasingPower, currentLocation, status, level, segment);
         getDataStore().save(user);
 
         ObjectNode result = Json.newObject();
@@ -62,41 +47,47 @@ public class UserController extends BaseController{
         return ok(result);
     }
 
-	public static Result getUser(String userId){
-    	Datastore datastore = getDataStore();
+    public static Result getUser(String userId) {
+        Datastore datastore = getDataStore();
         User user = datastore.get(User.class, new ObjectId(userId));
+        if(user == null){
+            return generateBadRequest("User not found");
+        }
         ObjectNode result = Json.newObject();
         result.put("data", user.toJson());
         return ok(result);
     }
 
-    public static Result addCart(){
-		if(!Utils.checkJsonInput(request())){
-			Logger.info("Register User. Bad request data for register user "+request().body());
-	    	return generateBadRequest("Bad input json" + request().body());
-		}
-		JsonNode jsonReq = request().body().asJson();
-		String userId = Utils.safeStringFromJson(jsonReq, "userId");
-        String list = jsonReq.get("list").asText();
-		
-		//add List<products>, amount, type)
-		//return agent
-	       
-	    Datastore datastore = getDataStore();
-        long latitude = Utils.safeLongFromJson(jsonReq, "lat");
-        long longitude = Utils.safeLongFromJson(jsonReq, "lon");
-        Point productLocation = new PointBuilder().latitude(latitude).longitude(longitude).build();
+    public static Result addCart() {
 
+        if (!Utils.checkJsonInput(request())) {
+            Logger.info("Register User. Bad request data for register user " + request().body());
+            return generateBadRequest("Bad input json" + request().body());
+        }
+
+        Datastore datastore = getDataStore();
+        JsonNode jsonReq = request().body().asJson();
+        String userId = Utils.safeStringFromJson(jsonReq, "userId");
+        User user = datastore.get(User.class, new ObjectId(userId));
+
+        user.setCurrentCart(new Cart());
+        for (JsonNode productItem : jsonReq.withArray("products")) {
+            String productId = Utils.safeStringFromJson(productItem, "productId");
+            int quantity = Utils.safeIntFromJson(productItem, "quantity", 1);
+            Product product = datastore.get(Product.class, new ObjectId(productId));
+            user.getCurrentCart().addToCart(product, quantity);
+        }
+        datastore.save(user);
         return generateOkTrue();
-	}
-	
-	public static Result updateLocation(){
-		if(!Utils.checkJsonInput(request())){
-			Logger.info("Register User. Bad request data for register user "+request().body());
-	    	return generateBadRequest("Bad input json" + request().body());
-		}
-		JsonNode jsonReq = request().body().asJson();
-		Datastore datastore = getDataStore();
+    }
+
+    public static Result updateLocation() {
+        if (!Utils.checkJsonInput(request())) {
+            Logger.info("Register User. Bad request data for register user " + request().body());
+            return generateBadRequest("Bad input json" + request().body());
+        }
+        JsonNode jsonReq = request().body().asJson();
+        Datastore datastore = getDataStore();
         String userId = Utils.safeStringFromJson(jsonReq, "userId");
         long latitude = Utils.safeLongFromJson(jsonReq, "lat");
         long longitude = Utils.safeLongFromJson(jsonReq, "lon");
@@ -109,19 +100,19 @@ public class UserController extends BaseController{
         ObjectNode result = Json.newObject();
         result.put("user", user.toJson());
         return ok(result);
-	}
-	
-	public static Result pay(){
-		if(!Utils.checkJsonInput(request())){
-			Logger.info("Register User. Bad request data for add pay "+request().body());
-	    	return generateBadRequest("Bad input json" + request().body());
-		}
-		JsonNode jsonReq = request().body().asJson();
-		String userId = jsonReq.get("userId").asText();
-		String token = jsonReq.get("token").asText();
+    }
+
+    public static Result pay() {
+        if (!Utils.checkJsonInput(request())) {
+            Logger.info("Register User. Bad request data for add pay " + request().body());
+            return generateBadRequest("Bad input json" + request().body());
+        }
+        JsonNode jsonReq = request().body().asJson();
+        String userId = jsonReq.get("userId").asText();
+        String token = jsonReq.get("token").asText();
 
         User user = getDataStore().get(User.class, new ObjectId(userId));
-        if(user == null){
+        if (user == null) {
             return generateBadRequest("Invalid userId");
         }
 
@@ -141,13 +132,13 @@ public class UserController extends BaseController{
         if ("APPROVED".equals(payment.get("paymentStatus"))) {
             try {
                 Invoice invoice = Invoice.create(new PaymentsMap()
-                    .set("currency", "USD")
-                    .set("email", user.getEmail())
-                    .set("items[0].amount", 5504L)
-                    .set("items[0].quantity", 1L)
-                    .set("name", user.getName())
-                    .set("note", "Thank you for shopping with GoCart")
-                    .set("suppliedDate", System.currentTimeMillis())
+                                .set("currency", "USD")
+                                .set("email", user.getEmail())
+                                .set("items[0].amount", 5504L)
+                                .set("items[0].quantity", 1L)
+                                .set("name", user.getName())
+                                .set("note", "Thank you for shopping with GoCart")
+                                .set("suppliedDate", System.currentTimeMillis())
                 );
 
             } catch (Exception e) {
@@ -157,6 +148,6 @@ public class UserController extends BaseController{
         }
 
         return generateOkTrue();
-	}
-	
+    }
+
 }
--- app/model/Cart.java
+++ app/model/Cart.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bson.types.ObjectId;
@@ -8,76 +9,75 @@ import org.mongodb.morphia.annotations.Id;
 import com.fasterxml.jackson.databind.JsonNode;
 import play.libs.Json;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
 
 @Entity("Carts")
 public class Cart {
 
-	@Id
+    @Id
     private ObjectId id;
 
-	private List<Product> products;
-	private long totalAmount;
-	private String type; //0 - self, 1 - agent
-	private boolean paid;
-	
-	public ObjectId getId() {
-		return id;
-	}
+    private List<Product> products;
+    private List<Integer> productsQuantity;
+    private String type; //0 - self, 1 - agent
+    private boolean paid;
 
-	public void setId(ObjectId id) {
-		this.id = id;
-	}
+    public Cart(){
+        products = new ArrayList<>();
+        productsQuantity = new ArrayList<>();
+    }
 
-   public List<Product> getProducts() {
-      return products;
-   }
+    public ObjectId getId() {
+        return id;
+    }
 
-   public void setProducts(List<Product> products) {
-      this.products = products;
-   }
+    public List<Product> getProducts() {
+        return products;
+    }
 
-   public long getTotalAmount() {
-      return totalAmount;
-   }
+    public void addToCart(Product product, int quantity){
+        products.add(product);
+        productsQuantity.add(quantity);
+    }
 
-   public void setTotalAmount(long totalAmount) {
-      this.totalAmount = totalAmount;
-   }
+    public long getTotalAmount() {
+        long totalAmount = 0;
+        for(int i = 0; i < products.size(); i++){
+            totalAmount = totalAmount + (products.get(i).getPrice() * productsQuantity.get(i));
+        }
+        return totalAmount;
+    }
 
-   public String getType() {
-      return type;
-   }
+    public String getType() {
+        return type;
+    }
 
-   public void setType(String type) {
-      this.type = type;
-   }
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	public boolean isPaid() {
-		return paid;
-	}
+    public boolean isPaid() {
+        return paid;
+    }
 
-	public void setPaid(boolean paid) {
-		this.paid = paid;
-	}
-	
-	public JsonNode toJson(){
-       ObjectNode result = Json.newObject();
-       result.put("id", getId().toString());
-       ArrayNode productArr = new ArrayNode(JsonNodeFactory.instance);
-		for(Product product : getProducts()){
-			productArr.add(product.toJson());
-		}
-	    result.put("products", productArr);
-       //result.put("products", getProducts.toJSON());
-       result.put("totalAmout", getTotalAmount());
-       result.put("type", getType());
-       result.put("paid", isPaid());
-       return result;
-   }
-	
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
+
+    public JsonNode toJson() {
+        ObjectNode result = Json.newObject();
+        result.put("id", getId().toString());
+        ArrayNode productArr = new ArrayNode(JsonNodeFactory.instance);
+        for (Product product : getProducts()) {
+            productArr.add(product.toJson());
+        }
+        result.put("products", productArr);
+        result.put("totalAmount", getTotalAmount());
+        result.put("type", getType());
+        result.put("paid", isPaid());
+        return result;
+    }
+
 }
